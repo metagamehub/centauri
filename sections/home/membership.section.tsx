860aaef9540a1009bcc1c9a8d2b44ac66d21eb91
Andres <andgonzalezcas@unal.edu.co>
2023-01-19 22:00:09 -0500

    responsive membership

--- sections/home/membership.section.tsx
+++ sections/home/membership.section.tsx
@@ -8,15 +8,15 @@ const Membership = () => {
     'Loylaty module and dynamic governance upgrades in shaping the direction of the consortium'
   ]
   return (
-    <div className="w-full flex items-center justify-between mb-44">
-      <h2 className="text-7xl font-normal max-w-2xl">YOUR CUSTOM CONSORTIUM MEMBERSHIP:</h2>
-      <div className="flex flex-col justify-center items-center bg-blured-floating max-w-2xl px-28 py-16">
+    <div className="w-full flex flex-col gap-36 xl:gap-0 xl:flex-row items-center 2xl:justify-center mb-44">
+      <h2 className="text-5xl sm:text-7xl font-normal max-w-2xl text-center xl:text-left">YOUR CUSTOM CONSORTIUM MEMBERSHIP:</h2>
+      <div className="flex flex-col justify-center items-center bg-blured-floating max-w-2xl px-10 sm:px-28 py-16">
         {/* Circle decorator */}
         <div className='absolute -top-[88px] w-44 h-44 flex justify-center items-center bg-blured rounded-full'>
           <Image src={`/asset-orator.svg`} alt={`metanode dao logo`} width={108} height={108} />
         </div>
 
-        <h3 className="text-5xl font-bold my-16">WEB3 ORATOR</h3>
+        <h3 className="text-5xl font-bold my-16 text-center sm:text-left">WEB3 ORATOR</h3>
         <ul className="list-disc text-2xl">
           {oratorList.map((item, index) => <li key={index}>{item}</li>)}
         </ul>
