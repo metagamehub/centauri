c9f01db8efa9a0bd223780c9fab2b54557d2a41b
Andres <andgonzalezcas@unal.edu.co>
2023-01-19 22:50:01 -0500

    responsive invitation and keyfeature on education view

--- sections/education/invitation.section.tsx
+++ sections/education/invitation.section.tsx
@@ -11,13 +11,13 @@ const CircleCard = ({ title, children }: { title: string, children: React.ReactN
 
 const Invitation = () => {
   return (
-    <div className='relative flex justify-center items-center gap-20'>
-      <div className="w-1/2">
-        <h2 className='text-[115px] font-bold leading-none'>JOIN THE REVOLUTION,</h2>
-        <p className="text-5xl my-8">Attend our seminars and transform the future.</p>
-        <p className="text-2xl font-light">Don't miss this exclusive opportunity to master the metaverse! Register now on our website and follow us on social media (link to twitter, linkedin, discord) for the latest updates on upcoming seminar dates, topics, and keynote speakers.</p>
+    <div className='relative flex flex-col xl:flex-row justify-around items-center gap-20'>
+      <div className="xl:w-2/3 flex flex-col justify-center xl:items-end text-center xl:text-right">
+        <h2 className='text-5xl sm:text-6xl lg:text-8xl 2xl:text-[115px] font-bold leading-none'>JOIN THE REVOLUTION,</h2>
+        <p className="text-2xl sm:text-4xl 2xl:text-5xl my-8">Attend our seminars and transform the future.</p>
+        <p className="text-base sm:text-xl 2xl:text-2xl font-light">Don't miss this exclusive opportunity to master the metaverse! Register now on our website and follow us on social media (link to twitter, linkedin, discord) for the latest updates on upcoming seminar dates, topics, and keynote speakers.</p>
       </div>
-      <div className='w-1/2 flex flex-col justify-center items-center gap-3'>
+      <div className='xl:w-1/3 flex flex-col justify-center items-start gap-10 scale-75 sm:scale-100'>
         <CircleCard title="WHEN?">
           <div className="my-3 text-2xl">
             <h3 className="font-bold">Date:</h3>
--- sections/education/keyfeature.section.tsx
+++ sections/education/keyfeature.section.tsx
@@ -11,12 +11,12 @@ const KeyFeatures = () => {
   ]
 
   return (
-    <div className='relative flex justify-center items-center bg-blured p-20 gap-20'>
-      <div className="absolute w-[181px] h-[181px] bg-blured-floating rounded-full -top-[90px] left-10 flex justify-center items-center">
+    <div className='relative flex flex-col lg:flex-row justify-center items-center bg-blured p-20 gap-20 min-w-[500px]'>
+      <div className="absolute w-[181px] h-[181px] bg-blured-floating rounded-full -top-[90px] left-20 flex justify-center items-center">
         <Image src={`/icons/target-shooting.svg`} alt={`shooting target icon`} width={102} height={102} />
       </div>
-      <h2 className='w-1/2 text-6xl font-medium'>Key features of our seminars include:</h2>
-      <ul className='w-1/2 text-2xl font-light list-disc'>
+      <h2 className='lg:w-1/2 pt-10 lg:pt-0 text-6xl font-medium'>Key features of our seminars include:</h2>
+      <ul className='lg:w-1/2 text-2xl font-light list-disc'>
         {featureList.map((feature, index) => <li key={index} className="">{feature}</li>)}
       </ul>
     </div>
