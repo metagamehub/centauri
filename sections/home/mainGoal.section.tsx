9fc4f7e1c2e0a1967e4bbf8e0c9927bd63cc6418
Andres <andgonzalezcas@unal.edu.co>
2023-01-19 21:27:52 -0500

    the main goal responsive

--- sections/home/mainGoal.section.tsx
+++ sections/home/mainGoal.section.tsx
@@ -1,10 +1,10 @@
 const MainGoal = () => {
   return (
-    <div className='w-full grid lg:grid-cols-2 justify-center items-center gap-28 my-36'>
-      <div className='w-full flex justify-center items-center'>
-        <h2 className='w-[545px] text-[7.25rem] font-extralight leading-none'>{'THE MAIN GOAL'}</h2>
+    <div className='w-full grid lg:grid-cols-2 gap-28 my-36'>
+      <div className='w-full'>
+        <h2 className='text-center text-8xl xl:text-[7.25rem] font-extralight leading-none'>{'THE MAIN GOAL'}</h2>
       </div>
-      <div className='flex flex-col gap-10'>
+      <div className='flex flex-col justify-center items-center gap-10 px-5 lg:p-0'>
         <p className='text-2xl max-w-3xl font-light leading-none'>{'The main goal of the Centauri Web3 Consortium is to foster the development and adoption of web3 through the creation of open and transparent standards to build a harmonized and scalable ecosystem with interoperability at its core.'}</p>
         <p className='text-2xl max-w-3xl font-light leading-none'>{'The Centauri Web3 Consortium is educating, standardizing and enabling web3 adoption for businesses and creators by building “The Metanode” hive.'}</p>
       </div>
